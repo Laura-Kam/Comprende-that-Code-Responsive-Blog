399e63031083e21015fcf9c23fc5b736564d3f1c
Laura Kambanis <kambanislaura@gmail.com>
2025-08-24 14:12:00 +0100

    bare minimum index.html markup added, dynamically generated header and footer created

---
+++ js/layout.mjs
@@ -0,0 +1,30 @@
+export function loadHeader() {
+  return `
+    <header class="header">
+    <div class="header__brand">
+      <img class="logo" />
+      <span class="header__title">CodeComprende</span>
+    </div>
+    <nav class="header__nav">
+      <ul class="header__nav-list">
+        <li><a href="index.html" class="header__nav-link">Home</a></li>
+        <li>
+          <a href="./blog-posts/about-me.html" class="header__nav-link"
+            >About</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+    `;
+}
+
+export function loadFooter() {
+  return `
+    <footer class="footer">
+    <p class="footer__heading">CodeComprende</p>
+    <img src="./images/CC.svg" class="logo" />
+    <p class="footer__copyright">Copyright@2022</p>
+  </footer>
+    `;
+}
