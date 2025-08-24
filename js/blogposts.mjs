1600a9894818e05e568efefe5e7484429863cfc1
Laura Kambanis <kambanislaura@gmail.com>
2025-08-24 15:16:47 +0100

    added blog post data

---
+++ js/blogposts.mjs
@@ -0,0 +1,49 @@
+export const blogPosts = [
+  {
+    id: 1,
+    title: "Responsive Design - Where do I even start?",
+    date: "July 23, 2025",
+    excerpt: "Flex-box, grid image sets...making sense of it all.",
+    image: "./images/digital-devices.png",
+    alt: "A desk with a laptop, pink desk lamp, plant, notebook, and tablet. It shows the importance of having responsive web pages for different screens.",
+  },
+  {
+    id: 2,
+    title: "Coding with Curiosity not Frustration",
+    date: "July 25, 2025",
+    excerpt:
+      "How I stopped chasing rigid goals and started looping my way to growth.",
+    image: "./images/coding-frustration.png",
+    alt: "A frustrated person at a laptop, holding a tense expression. A smartphone and a mug sit beside them",
+    content: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. 
+    Tempus leo eu aenean sed diam urna tempor. 
+    Pulvinar vivamus fringilla 
+    lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+    Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem 
+    placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed 
+    diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl 
+    malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora 
+    torquent per conubia nostra inceptos himenaeos.`,
+  },
+  {
+    id: 3,
+    title: "Is Scrimba The Best Online Coding Platform?",
+    date: "July 23, 2025",
+    excerpt: "I wanted to find out for myself if it was worth the hype.",
+    image: "./images/scrimba-post.png",
+    alt: "Scrimba post preview image",
+    content: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. 
+    Tempus leo eu aenean sed diam urna tempor. 
+    Pulvinar vivamus fringilla 
+    lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+    Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem 
+    placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed 
+    diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl 
+    malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora 
+    torquent per conubia nostra inceptos himenaeos.`,
+  },
+];
