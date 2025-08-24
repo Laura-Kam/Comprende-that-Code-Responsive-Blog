56c8eb44905609eb2986077ea4b48647a319f1cc
Laura Kambanis <kambanislaura@gmail.com>
2025-08-24 17:34:20 +0100

    grid-base styles created

--- js/main.mjs
+++ js/main.mjs
@@ -20,4 +20,30 @@ heroSection.innerHTML = `
   </div>
 `;
 
+document.addEventListener("DOMContentLoaded", () => {
+  const blogList = document.querySelector(".grid");
+
+  blogPosts.forEach((post) => {
+    const article = document.createElement("article");
+    article.className = "card";
+    article.dataset.id = post.id;
+
+    article.innerHTML = `
+        <img
+          class="card__image"
+          src="${post.image}"
+          alt="${post.alt}"
+        />
+        <div class="card-info">
+          <p class="card__date">${post.date}</p>
+          <h2 class="card__title">${post.title}</h2>
+        </div>
+        <p class="card__extract">${post.excerpt}</p>
+        </article>
+      `;
+
+    blogList.appendChild(article);
+  });
+});
+
 console.log(mainPost);
