72dbff2f9c200b82d4052de7a5dbab5fe062e3e4
Laura Kambanis <kambanislaura@gmail.com>
2025-08-27 16:51:20 +0100

    main post rendering and articles page styled and rendered

--- js/article.mjs
+++ js/article.mjs
@@ -1,5 +1,5 @@
 import { loadHeader, loadFooter } from "./layout.mjs";
-import { blogPosts } from "./blogposts.mjs";
+import { blogPosts, mainPost } from "./blogposts.mjs";
 
 document.getElementById("header-placeholder").innerHTML = loadHeader();
 document.getElementById("footer-placeholder").innerHTML = loadFooter();
@@ -12,28 +12,41 @@ const params = new URLSearchParams(window.location.search);
 //get the id number from it
 const blogId = parseInt(params.get("id"), 10);
 
+let blog;
 //loop through blogposts and find the matching id
-const blog = blogPosts.find((b) => b.id === blogId);
+if (blogId === mainPost[0].id) {
+  blog = mainPost[0];
+} else {
+  blog = blogPosts.find((b) => b.id === blogId);
+}
 
 if (blog) {
   fullArticleContainer.innerHTML = `
-  <div class="article-heading-container">
-  <div class="article__info">
+  <section class="article-intro-section">
+  <div class="article-date-title-container">
     <p class="article__date">${blog.date}</p>
     <h1 class="article__title">${blog.title}</h1>
   </div>
-  <p class="hero__blog-extract">${blog.excerpt}</p>
-  <img src="${blog.image}"/>
-</div>
-<section class="article-main-content">
+  <p class="article__extract">${blog.excerpt}</p>
+  <img class="article__image" src="${blog.image}"/>
+  </section>
 
-<h2 class="article-main-content__subheading">${blog.subheading1}</h2>
-<p lass="article-main-content__paragraph">${blog.paragraph1}</p>
-<h2 class="article-main-content__subheading">${blog.subheading2}</h2>
-<p lass="article-main-content__paragraph">${blog.paragraph2}</p>
-<h2 class="article-main-content__subheading">${blog.subheading3}</h2>
-<p lass="article-main-content__paragraph">${blog.paragraph3}</p>
+  <section class="article-main-content">
+  <div class="paragraph-section">
+<h2 class="article__subheading">${blog.subheading1}</h2>
+<p lass="article__paragraph">${blog.paragraph1}</p>
+</div>
+<div class="paragraph-section">
+    <h2 class="article__subheading">${blog.subheading2}</h2>
+    <p lass="article__paragraph">${blog.paragraph2}</p>
+</div>
+<div class="paragraph-section">
+<h2 class="article__subheading">${blog.subheading3}</h2>
+<p lass="article__paragraph">${blog.paragraph3}</p>
+</div>
 </section>
+
+
 
     `;
 } else {
